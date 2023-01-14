0b4833edb81b77c8596bd24544df5086c6a35b40
SSH TURBO <64099400+universoflix@users.noreply.github.com>
2023-01-14 00:40:25 -0300

    Add files via upload

---
+++ apipagamento.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/spark22332/SSHPLUS/master/remove.sh
+chmod 777 remove.sh
---
+++ sshplusinst.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+wget https://raw.githubusercontent.com/spark22332/SSHPLUS/master/remove.sh
+chmod 777 remove.sh
+wget https://raw.githubusercontent.com/spark22332/sshplusapi/main/AtlantusMakeAccount.sh
+chmod 777 AtlantusMakeAccount.sh
+mkdir -m 777 /var/www/html/pagamento
+cd /var/www/html/pagamento && wget https://sshturbo.com/sshplus.zip && unzip sshplus.zip
+echo -e " \e[0;31mInstalado com Sucesso \e[0m"
