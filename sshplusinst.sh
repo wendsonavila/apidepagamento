42f2a747400229a69cc93c5d6107621db069dc9e
SSH TURBO <64099400+universoflix@users.noreply.github.com>
2023-02-01 20:46:56 -0300

    Update sshplusinst.sh

--- sshplusinst.sh
+++ sshplusinst.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 wget https://raw.githubusercontent.com/spark22332/SSHPLUS/master/remove.sh
 chmod 777 remove.sh
-wget https://raw.githubusercontent.com/spark22332/sshplusapi/main/AtlantusMakeAccount.sh
+wget https://raw.githubusercontent.com/universoflix/painel/main/AtlantusMakeAccount.sh
 chmod 777 AtlantusMakeAccount.sh
 mkdir -m 777 /var/www/html/pagamento
 cd /var/www/html/pagamento && wget https://sshturbo.com/api-sshplus.zip && unzip api-sshplus.zip
