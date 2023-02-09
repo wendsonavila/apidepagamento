c0d6bbd173dd33aeaede9c72718b825ac052794f
SSH TURBO <64099400+universoflix@users.noreply.github.com>
2023-02-09 14:36:06 -0300

    Update api-sshplus-install.sh

--- api-sshplus-install.sh
+++ api-sshplus-install.sh
@@ -2,7 +2,7 @@
 wget https://raw.githubusercontent.com/universoflix/painel/main/remove.sh
 chmod 777 remove.sh
 wget https://raw.githubusercontent.com/universoflix/painel/main/sshturboMakeAccount.sh
-chmod 777 AtlantusMakeAccount.sh
+chmod 777 sshturboMakeAccount.sh
 mkdir -m 777 /var/www/html/pagamento
 cd /var/www/html/pagamento && wget https://raw.githubusercontent.com/universoflix/painel/main/api-sshplus.zip && unzip api-sshplus.zip
 echo -e " \e[0;31mInstalado com Sucesso \e[0m"
