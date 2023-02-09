40be62d03a058e431ab0b9a6260e542394d9e8c2
SSH TURBO <64099400+universoflix@users.noreply.github.com>
2023-02-09 14:37:05 -0300

    Create servidor.sh

---
+++ servidor.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+wget https://raw.githubusercontent.com/universoflix/painel/main/remove.sh
+chmod 777 remove.sh
+wget https://raw.githubusercontent.com/universoflix/painel/main/sshturboMakeAccount.sh
+chmod 777 sshturboMakeAccount.sh
+echo -e " \e[0;31mInstalado com Sucesso \e[0m"
