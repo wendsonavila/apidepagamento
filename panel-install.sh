83da315517f6a1137da34cdccb24dfd383d63375
Jefferson <64099400+universoflix@users.noreply.github.com>
2023-02-10 12:45:24 -0300

    Add files via upload

---
+++ panel-install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "\033[1;36mRemovendo e Instalando o PHP7.2 \033[0m"
+wget https://raw.githubusercontent.com/universoflix/php/main/removendo-e-instalando.sh && chmod 777 removendo-e-instalando.sh && ./removendo-e-instalando.sh
+echo "\033[1;36mInstalação do ioncube \033[0m"
+wget https://raw.githubusercontent.com/universoflix/php/main/install-ioncub.sh && chmod 777 install-ioncub.sh && ./install-ioncub.sh   
+echo "\033[1;36mInstalando o subdominio \033[0m" 
+wget https://raw.githubusercontent.com/universoflix/php/main/install-sub.sh && chmod 777 install-sub.sh && ./install-sub.sh
+echo "\033[1;36mInstalando Painel de Renovação \033[0m" 
+wget https://raw.githubusercontent.com/universoflix/painel/main/sshplus-api-install.sh && chmod 777 sshplus-api-install.sh && ./sshplus-api-install.sh
+echo "\e[0;31mInstalado com Sucesso \e[0m"
---
+++ sshplus-api-install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+wget https://raw.githubusercontent.com/universoflix/painel/main/remove.sh
+chmod 777 remove.sh
+wget https://raw.githubusercontent.com/universoflix/painel/main/AtlantusMakeAccount.sh
+chmod 777 AtlantusMakeAccount.sh
+mkdir -m 777 /var/www/renovar
+cd /var/www/renovar && wget https://raw.githubusercontent.com/universoflix/painel/main/api-sshplus.zip && unzip api-sshplus.zip
+echo " \e[0;31mInstalado com Sucesso \e[0m"
