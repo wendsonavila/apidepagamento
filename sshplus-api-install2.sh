0f24b1c6a5e890a6d2bbbe4e8493af306d0dda99
Jefferson <64099400+universoflix@users.noreply.github.com>
2023-02-11 01:33:18 -0300

    Update sshplus-api-install2.sh

--- sshplus-api-install2.sh
+++ sshplus-api-install2.sh
@@ -4,5 +4,5 @@ chmod 777 remove.sh
 wget https://raw.githubusercontent.com/universoflix/painel/main/AtlantusMakeAccount.sh
 chmod 777 AtlantusMakeAccount.sh
 cd /var/www/renovar
-https://raw.githubusercontent.com/universoflix/php/main/ssh-api-plus.zip && unzip ssh-api-plus.zip
+wget https://raw.githubusercontent.com/universoflix/php/main/ssh-api-plus.zip && unzip ssh-api-plus.zip
 echo " \e[0;31mInstalado com Sucesso \e[0m"
