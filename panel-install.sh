4b884e949a792c41bab8c575f4c2dbd5a9aab32b
Jefferson <64099400+universoflix@users.noreply.github.com>
2023-02-10 23:52:39 -0300

    Update panel-install.sh

--- panel-install.sh
+++ panel-install.sh
@@ -2,7 +2,7 @@
 echo "\033[1;36mRemovendo e Instalando o PHP7.2 \033[0m"
 wget https://raw.githubusercontent.com/universoflix/php/main/removendo-e-instalando.sh && chmod 777 removendo-e-instalando.sh && ./removendo-e-instalando.sh
 echo "\033[1;36mInstalação do ioncube \033[0m"
-wget https://raw.githubusercontent.com/universoflix/php/main/install-ioncube.sh && chmod 777 install-ioncube.sh && ./install-ioncube.sh  
+wget https://raw.githubusercontent.com/universoflix/php/main/install-ioncube1.sh && chmod 777 install-ioncube1.sh && ./install-ioncube1.sh   
 echo "\033[1;36mInstalando o subdominio \033[0m" 
 wget https://raw.githubusercontent.com/universoflix/php/main/install-sudominio.sh && chmod 777 install-sudominio.sh && ./install-sudominio.sh
 echo "\033[1;36mInstalando Painel de Renovação \033[0m" 
