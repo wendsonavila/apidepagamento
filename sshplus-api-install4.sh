5afe6816d4f4d3c27604a6def401f7acb30ae0b6
Jefferson <64099400+universoflix@users.noreply.github.com>
2023-02-19 21:25:52 -0300

    Update and rename sshplus-api-install3.sh to sshplus-api-install4.sh

--- sshplus-api-install4.sh
+++ sshplus-api-install4.sh
@@ -2,7 +2,7 @@
 wget https://raw.githubusercontent.com/universoflix/painel/main/remove.sh
 chmod 777 remove.sh
 wget https://raw.githubusercontent.com/universoflix/painel/main/AtlantusMakeAccount.sh
-chmod 777 AtlantusMakeAccount.sh
+chmod 777 AtlantusMakeAccountForce.sh
 cd /var/www/renovar
-wget https://raw.githubusercontent.com/universoflix/php/main/ssh-api-plus.zip && unzip ssh-api-plus.zip
+wget https://raw.githubusercontent.com/universoflix/painel/main/API-V1.zip && unzip API-V1.zip
 echo " \e[0;31mInstalado com Sucesso \e[0m"
