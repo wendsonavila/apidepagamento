1e7d06967154d1375168f0014f4cca4d0c5428d3
Jefferson <64099400+universoflix@users.noreply.github.com>
2023-02-21 18:27:17 -0300

    Update sshplus-api-install4.sh

--- sshplus-api-install4.sh
+++ sshplus-api-install4.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 wget https://raw.githubusercontent.com/universoflix/painel/main/remove.sh
 chmod 777 remove.sh
-wget https://raw.githubusercontent.com/universoflix/painel/main/AtlantusMakeAccount.sh
+wget https://raw.githubusercontent.com/universoflix/painel/main/AtlantusMakeAccountForce.sh
 chmod 777 AtlantusMakeAccountForce.sh
 cd /var/www/renovar
 wget https://raw.githubusercontent.com/universoflix/painel/main/API-V2-3-Criptografado.zip && unzip API-V2-3-Criptografado.zip
