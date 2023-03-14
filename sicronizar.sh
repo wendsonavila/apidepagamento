69b9df4b5d4350eba5867885c0abeac16815e544
wendsonavila <103389432+wendsonavila@users.noreply.github.com>
2023-03-14 13:23:19 -0300

    Update sicronizar.sh

--- sicronizar.sh
+++ sicronizar.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-wget https://raw.githubusercontent.com/universoflix/painel/main/remove.sh
+wget https://raw.githubusercontent.com/wendsonavila/apidepagamento/main/remove.sh
 chmod 777 remove.sh
-wget https://raw.githubusercontent.com/universoflix/painel/main/AtlantusMakeAccount.sh
+wget https://raw.githubusercontent.com/wendsonavila/apidepagamento/main/AtlantusMakeAccountForce.sh
 chmod 777 AtlantusMakeAccount.sh
 echo -e " \e[0;31mInstalado com Sucesso \e[0m"
